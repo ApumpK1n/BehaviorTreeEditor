2af3584cd331a91975f388ddba73974fb4f6db34
ApumpK1n <lsmzng@163.com>
2022-11-13 23:19:39 +0800

    xd

---
+++ Assets/BehaviorTree/Editor/Core/Config/BTGraphActionConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pumpkin.AI.BehaviorTree
+{
+    public class BTGraphActionConfig : ScriptableObject
+    {
+        
+
+    }
+}
